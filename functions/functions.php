2b9f4a5222c19679b8db6994ff28d38ada89f32f
Waqar Hussain <99593930+waqar-hussain-khushk@users.noreply.github.com>
2022-02-17 13:58:43 +0500

    Add files via upload

--- functions/functions.php
+++ functions/functions.php
@@ -7,7 +7,7 @@
 
 <?php
 
-$db = mysqli_connect("localhost","root","","refference_db");
+$db = mysqli_connect("localhost","root","","ecom_store");
 
 /// IP address code starts /////
 function getRealUserIp(){
@@ -19,7 +19,6 @@ function getRealUserIp(){
     }
  }
 /// IP address code Ends /////
-
 
 // items function Starts ///
 
@@ -62,11 +61,20 @@ $pro_id = $record['p_id'];
 
 $pro_qty = $record['qty'];
 
+$get_price = "select * from products where product_id='$pro_id'";
 
-$sub_total = $record['p_price']*$pro_qty;
+$run_price = mysqli_query($db,$get_price);
+
+while($row_price=mysqli_fetch_array($run_price)){
+
+
+$sub_total = $row_price['product_price']*$pro_qty;
 
 $total += $sub_total;
 
+
+
+}
 
 
 
@@ -74,8 +82,7 @@ $total += $sub_total;
 
 }
 
-echo "Rs" . $total;
-
+echo "$" . $total;
 
 
 }
@@ -84,7 +91,6 @@ echo "Rs" . $total;
 
 // total_price function Ends //
 
-// getPro function Starts //
 
 function getPro(){
 
@@ -104,28 +110,13 @@ $pro_price = $row_products['product_price'];
 
 $pro_img1 = $row_products['product_img1'];
 
-$product_label = "
-
-<a class='label sale' href='#' style='color:black;'>
-
-<div class='thelabel'>New</div>
-
-<div class='label-background'> </div>
-
-</a>
-
-";
-
-
-
-
 echo "
 
-<div class='col-md-4 col-sm-6 single' id='single'>
+<div class='col-md-4 col-sm-6 single' >
 
 <div class='product' >
 
-<a href='$pro_id' >
+<a href='details.php?pro_id=$pro_id' >
 
 <img src='admin_area/product_images/$pro_img1' class='img-responsive' >
 
@@ -133,17 +124,15 @@ echo "
 
 <div class='text' >
 
-<hr>
+<h3><a href='details.php?pro_id=$pro_id' >$pro_title</a></h3>
 
-<h3><a href='$pro_id' >$pro_title</a></h3>
-
-<p class='price' > Rs.$pro_price </p>
+<p class='price' >$ $pro_price</p>
 
 <p class='buttons' >
 
-<a href='$pro_id' class='btn btn-default' >View details</a>
+<a href='details.php?pro_id=$pro_id' class='btn btn-default' >View details</a>
 
-<a href='$pro_id' style='color: #000; background: #facc04; border: 1px solid #000;' class='btn btn-primary'>
+<a href='details.php?pro_id=$pro_id' class='btn btn-primary'>
 
 <i class='fa fa-shopping-cart'></i> Add to cart
 
@@ -154,8 +143,6 @@ echo "
 
 </div>
 
-$product_label
-
 
 </div>
 
@@ -163,165 +150,13 @@ $product_label
 
 ";
 
-}
 
 
-
-// getPro function Ends //
-
-
-/// getProducts Function Starts ///
-
-function getProducts(){
-
-/// getProducts function Code Starts ///
-
-global $db;
-
-$aWhere = array();
-
-
-
-/// Products Categories Code Starts ///
-
-if(isset($_REQUEST['p_cat'])&&is_array($_REQUEST['p_cat'])){
-
-foreach($_REQUEST['p_cat'] as $sKey=>$sVal){
-
-if((int)$sVal!=0){
-
-$aWhere[] = 'p_cat_id='.(int)$sVal;
-
-}
-
-}
-
-}
-
-/// Products Categories Code Ends ///
-
-/// Categories Code Starts ///
-
-if(isset($_REQUEST['cat'])&&is_array($_REQUEST['cat'])){
-
-foreach($_REQUEST['cat'] as $sKey=>$sVal){
-
-if((int)$sVal!=0){
-
-$aWhere[] = 'cat_id='.(int)$sVal;
-
-}
-
-}
-
-}
-
-/// Categories Code Ends ///
-
-$per_page=6;
-
-if(isset($_GET['page'])){
-
-$page = $_GET['page'];
-
-}else {
-
-$page=1;
-
-}
-
-$start_from = ($page-1) * $per_page ;
-
-$sLimit = " order by 1 DESC LIMIT $start_from,$per_page";
-
-$sWhere = (count($aWhere)>0?' WHERE '.implode(' or ',$aWhere):'').$sLimit;
-
-$get_products = "select * from products  ".$sWhere;
-
-$run_products = mysqli_query($db,$get_products);
-
-while($row_products=mysqli_fetch_array($run_products)){
-
-$pro_id = $row_products['product_id'];
-
-$pro_title = $row_products['product_title'];
-
-$pro_price = $row_products['product_price'];
-
-$pro_img1 = $row_products['product_img1'];
-
-$product_label = "
-
-<a class='label sale' href='#' style='color:black;'>
-
-<div class='thelabel'>New</div>
-
-<div class='label-background'> </div>
-
-</a>
-
-";
 
 }
 
 
-echo "
-<div class='row'>
-<div class='col-lg-6 col-sm-12 center-responsive' >
-
-<div class='product' >
-
-<a href='$pro_id' >
-
-<img src='admin_area/product_images/$pro_img1' class='img-responsive' >
-
-</a>
-
-<div class='text' >
-
-
-<hr>
-
-<h3><a href='$pro_id' >$pro_title</a></h3>
-
-<p class='price' > $product_price</p>
-
-<p class='buttons' >
-
-<a href='$pro_id' class='btn btn-default' >View details</a>
-
-<a href='$pro_id' class='btn btn-primary'>
-
-<i class='fa fa-shopping-cart' data-price=$pro_price></i> Add to cart
-
-</a>
-
-
-</p>
-
-</div>
-</div>
-
-$product_label
-
-
-</div>
-
-</div>
-
-";
-
 }
-/// getProducts function Code Ends ///
-
-
-
-
-
-
-/// getProducts Function Ends ///
-
-
 /// getPaginator Function Starts ///
 
 function getPaginator(){
@@ -411,350 +246,5 @@ echo "' >".'Last Page'."</a></li>";
 }
 
 /// getPaginator Function Ends ///
-
-}
-
-
-
-// getSpl function Starts //
-
-function getSpl(){
-
-global $db;
-
-$get_products = "select * from products where cat_id=7";
-
-$run_products = mysqli_query($db,$get_products);
-
-while($row_products=mysqli_fetch_array($run_products)){
-
-$pro_id = $row_products['product_id'];
-
-$pro_title = $row_products['product_title'];
-
-$pro_price = $row_products['product_price'];
-
-$pro_img1 = $row_products['product_img1'];
-
-$product_label = "
-
-<a class='label sale' href='#' style='color:black;'>
-
-<div class='thelabel'>New</div>
-
-<div class='label-background'> </div>
-
-</a>
-
-";
-
-
-
-
-echo "
-
-<div class='col-md-4 col-sm-6 single' >
-
-<div class='product' >
-
-<a href='$pro_id' >
-
-<img src='admin_area/product_images/$pro_img1' class='img-responsive' >
-
-</a>
-
-<div class='text' >
-
-<hr>
-
-<h3><a href='$pro_id' >$pro_title</a></h3>
-
-<p class='price' > Rs.$pro_price </p>
-
-<p class='buttons' >
-
-<a href='$pro_id' class='btn btn-default' >View details</a>
-
-<a href='$pro_id' style='color: #000; background: #facc04; border: 1px solid #000;' class='btn btn-primary'>
-
-<i class='fa fa-shopping-cart'></i> Add to cart
-
-</a>
-
-
-</p>
-
-</div>
-
-$product_label
-
-
-</div>
-
-</div>
-
-";
-
-}
-}
-
-
-// getSpl function Ends //
-
-// getMen function Starts //
-
-function getMen(){
-
-global $db;
-
-$get_products = "select * from products where cat_id=5";
-
-$run_products = mysqli_query($db,$get_products);
-
-while($row_products=mysqli_fetch_array($run_products)){
-
-$pro_id = $row_products['product_id'];
-
-$pro_title = $row_products['product_title'];
-
-$pro_price = $row_products['product_price'];
-
-$pro_img1 = $row_products['product_img1'];
-
-$product_label = "
-
-<a class='label sale' href='#' style='color:black;'>
-
-<div class='thelabel'>New</div>
-
-<div class='label-background'> </div>
-
-</a>
-
-";
-
-
-
-
-echo "
-
-<div class='col-md-4 col-sm-6 single' >
-
-<div class='product' >
-
-<a href='$pro_id' >
-
-<img src='admin_area/product_images/$pro_img1' class='img-responsive' >
-
-</a>
-
-<div class='text' >
-
-<hr>
-
-<h3><a href='$pro_id' >$pro_title</a></h3>
-
-<p class='price' > Rs.$pro_price </p>
-
-<p class='buttons' >
-
-<a href='$pro_id' class='btn btn-default' >View details</a>
-
-<a href='$pro_id' style='color: #000; background: #facc04; border: 1px solid #000;' class='btn btn-primary'>
-
-<i class='fa fa-shopping-cart'></i> Add to cart
-
-</a>
-
-
-</p>
-
-</div>
-
-$product_label
-
-
-</div>
-
-</div>
-
-";
-
-}
-}
-
-
-// getMen function Ends //
-
-// getkid function Starts //
-
-function getKid(){
-
-global $db;
-
-$get_products = "select * from products where cat_id=3";
-
-$run_products = mysqli_query($db,$get_products);
-
-while($row_products=mysqli_fetch_array($run_products)){
-
-$pro_id = $row_products['product_id'];
-
-$pro_title = $row_products['product_title'];
-
-$pro_price = $row_products['product_price'];
-
-$pro_img1 = $row_products['product_img1'];
-
-$product_label = "
-
-<a class='label sale' href='#' style='color:black;'>
-
-<div class='thelabel'>New</div>
-
-<div class='label-background'> </div>
-
-</a>
-
-";
-
-
-
-
-echo "
-
-<div class='col-md-4 col-sm-6 single' >
-
-<div class='product' >
-
-<a href='$pro_id' >
-
-<img src='admin_area/product_images/$pro_img1' class='img-responsive' >
-
-</a>
-
-<div class='text' >
-
-<hr>
-
-<h3><a href='$pro_id' >$pro_title</a></h3>
-
-<p class='price' > Rs.$pro_price </p>
-
-<p class='buttons' >
-
-<a href='$pro_id' class='btn btn-default' >View details</a>
-
-<a href='$pro_id' style='color: #000; background: #facc04; border: 1px solid #000;' class='btn btn-primary'>
-
-<i class='fa fa-shopping-cart'></i> Add to cart
-
-</a>
-
-
-</p>
-
-</div>
-
-$product_label
-
-
-</div>
-
-</div>
-
-";
-
-}
-}
-
-
-// getKid function Ends //
-
-// getWomen function Starts //
-
-function getWomen(){
-
-global $db;
-
-$get_products = "select * from products where cat_id=2";
-
-$run_products = mysqli_query($db,$get_products);
-
-while($row_products=mysqli_fetch_array($run_products)){
-
-$pro_id = $row_products['product_id'];
-
-$pro_title = $row_products['product_title'];
-
-$pro_price = $row_products['product_price'];
-
-$pro_img1 = $row_products['product_img1'];
-
-$product_label = "
-
-<a class='label sale' href='#' style='color:black;'>
-
-<div class='thelabel'>New</div>
-
-<div class='label-background'> </div>
-
-</a>
-
-";
-
-
-
-
-echo "
-
-<div class='col-md-4 col-sm-6 single' >
-
-<div class='product' >
-
-<a href='$pro_id' >
-
-<img src='admin_area/product_images/$pro_img1' class='img-responsive' >
-
-</a>
-
-<div class='text' >
-
-<hr>
-
-<h3><a href='$pro_id' >$pro_title</a></h3>
-
-<p class='price' > Rs.$pro_price </p>
-
-<p class='buttons' >
-
-<a href='$pro_id' class='btn btn-default' >View details</a>
-
-<a href='$pro_id' style='color: #000; background: #facc04; border: 1px solid #000;' class='btn btn-primary'>
-
-<i class='fa fa-shopping-cart'></i> Add to cart
-
-</a>
-
-
-</p>
-
-</div>
-
-$product_label
-
-
-</div>
-
-</div>
-
-";
-
-}
-}
-
-
-// getWomen function Ends //
-
 
 ?>
